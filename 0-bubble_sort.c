d6fab9d4eb4ec6a5cc0a886b9e1b56ec5be9776a
Aishat452 <aishadeperfectly@gmail.com>
2023-07-28 16:03:02 -0700

    insertion sort lists

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,42 +1,43 @@
 #include "sort.h"
 
 /**
- * swap - Function that swaps the values of two variables.
- * @a: first variables pointer.
- * @b: second variables pointer.
- * @tmp: third temporary variable
- * Return: void
- */
+* swap - Function that swaps the values of two variables.
+* @a: first variables pointer.
+* @b: second variables pointer.
+* @tmp: third temporary variable
+* Return: void
+*/
 
 void swap(int *a, int *b)
 {
-	int tmp;
+int tmp;
 
-	tmp = *a;
-	*a = *b;
-	*b = tmp;
+tmp = *a;
+*a = *b;
+*b = tmp;
 }
 /**
- * bubble_sort - sorting an array elements
- * @array: the Array
- * @size: the Arrays size
- */
+* bubble_sort - sorting an array elements
+*:%s/^\s\+// | %s/\s\+$// | gg=G
+@array: the Array
+* @size: the Arrays size
+*/
 void bubble_sort(int *array, size_t size)
 {
-	size_t i, j;
+size_t i, j;
 
-	if (array == NULL && size == 0)
-		return;
+if (array == NULL && size == 0)
+return;
 
-	for (i = 0; i < size - 1; ++i)
-	{
-		for (j = 0; j < size - i - 1; ++j)
-		{
-			if (array[j] > array[j + 1])
-			{
-				swap(&array[j], &array[j + 1]);
-				print_array(array, size);
-			}
-		}
-	}
+for (i = 0; i < size - 1; ++i)
+{
+for (j = 0; j < size - i - 1; ++j)
+{
+if (array[j] > array[j + 1])
+{
+swap(&array[j], &array[j + 1]);
+print_array(array, size);
+}
+}
+}
 }
